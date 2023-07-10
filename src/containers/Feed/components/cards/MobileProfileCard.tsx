6f2b30d143b042bf435f475899aff093c273bc75
neoteny1010 <138466478+neoteny1010@users.noreply.github.com>
2023-07-10 11:59:12 +0900

    Update MobileProfileCard.tsx

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -17,7 +17,9 @@ const MobileProfileCard: React.FC<Props> = () => {
             width={90}
             height={90}
             alt="profile_image"
-            className="relative"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-2xl"
           />
           <div className="h-fit dark:text-white">
             <div className=" text-xl  italic font-bold">
