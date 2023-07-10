dfc280d4651d5d4462ea606cb4ff4dd66e2a3378
neoteny1010 <138466478+neoteny1010@users.noreply.github.com>
2023-07-10 11:30:24 +0900

    Update CommentBox.tsx

--- src/containers/Detail/components/PostDetail/CommentBox.tsx
+++ src/containers/Detail/components/PostDetail/CommentBox.tsx
@@ -24,7 +24,7 @@ const CommentBox: React.FC<Props> = ({ data }) => {
     <div>
       {CONFIG.utterances.enable && <UtterancesComponent issueTerm={data.id} />}
       {CONFIG.cusdis.enable && (
-        <CusdisComponent id={data.id} slug={data.slug} title={data.title} />
+        <CusdisComponent id={data.id} slug={data.slug} title={data.title} classname="cusdis-custom"/>
       )}
     </div>
   )
