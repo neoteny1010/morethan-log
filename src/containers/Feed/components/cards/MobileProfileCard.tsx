3adda5693410b58b00dbe24e252e21c397040b8c
neoteny1010 <138466478+neoteny1010@users.noreply.github.com>
2023-07-10 13:41:53 +0900

    Update MobileProfileCard.tsx

--- src/containers/Feed/components/cards/MobileProfileCard.tsx
+++ src/containers/Feed/components/cards/MobileProfileCard.tsx
@@ -19,7 +19,7 @@ const MobileProfileCard: React.FC<Props> = () => {
                 layout="fill"
                 objectFit="cover"
                 alt="profile_image"
-                className="image-smooth trnasform scale-110 trnaslate-x-2 translate-y-2"
+                className="image-smooth trnasform scale-150 trnaslate-x-1 translate-y-1"
               />
             </div>
           </div>
